8fc74cc3b7a289f7fd5803cdf3fcc8046cc08fc6
memoceydev-blip <memoceydev@gmail.com>
2026-05-22 23:06:59 +0300

    Add missing support files for asset portal

---
+++ frontend/public/config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  API_BASE_URL: "http://localhost:8000",
+  KEYCLOAK_URL: "https://keycloak.example.com",
+  KEYCLOAK_REALM: "master",
+  KEYCLOAK_CLIENT_ID: "asset-frontend"
+};
